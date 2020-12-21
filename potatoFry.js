8f1125b5efe94e9d82e73da7a3c1d40bdecbe1a2
dhanush14327 <73987909+dhanush14327@users.noreply.github.com>
2020-12-21 20:28:36 +0530

    Add files via upload

---
+++ potatoFry.js
@@ -0,0 +1,18 @@
+Preparation{
+    Wash potatoes well.
+    Peel them and cube to bite sizes or about half inch cubes.
+    Immerse them in a bowl of water until used further.
+    Crush or chop garlic and rinse the curry leaves.
+}
+
+How to Make{
+    Heat a pan with oil,add mustard,cumin and urad dal,when the dal gets lightly roasted add red chili,garlic and curry leaves.
+    Add potatoes and turmeric.
+    Fry for about 3 to 4 minutes.
+    Lower the flame.
+    Cook covered till the potatoes are soft cooked.
+    If needed you can sprinkle little water and cook.
+    Add red chili powder and garam masala.
+    Mix and fry for few more minutes,till the chili powder blends well with potatoes.
+    Serve aloo fry with rice and rasam.
+}
